f04561be91688872baaa0603c9eb5b2d49a1351b
kristupas <108592864+removeris@users.noreply.github.com>
2024-09-14 16:40:53 +0300

    Create a function to draw a new grid through user input

--- logic.js
+++ logic.js
@@ -1,15 +1,21 @@
-function createGrid(){
+function createGrid(gridSize){
     const container = document.querySelector("#container");
 
-    console.log(container.getAttribute("width"));
+    let gridCellSize = Math.floor(container.offsetHeight / gridSize) - 2;
 
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
+    console.log(gridCellSize);
+
+    for(let i = 0; i < gridSize; i++){
+        for(let j = 0; j < gridSize; j++){
             const gridCell = document.createElement("div");
             gridCell.classList.add("grid-cell");
+            gridCell.style.width = gridCellSize + "px";
+            gridCell.style.height = gridCellSize + "px";
             container.appendChild(gridCell);
         }
     }
+
+    changeCellColor();
 }
 
 function changeCellColor() {
@@ -22,5 +28,28 @@ function changeCellColor() {
     })
 }
 
-createGrid();
-changeCellColor();
+function createNewGrid() {
+    const button = document.querySelector("#new-grid");
+
+    button.addEventListener("click", () => {
+        removeCurrentGrid();
+
+        do{
+            newGridSize = parseInt(prompt("Enter how many blocks should be in a single row", "0"));
+        } while(newGridSize > 100 || !Number(newGridSize) || newGridSize < 0);
+
+        createGrid(newGridSize);
+        changeCellColor();
+    })
+}
+
+function removeCurrentGrid() {
+    const container = document.querySelector("#container");
+
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+createGrid(16);
+createNewGrid();
